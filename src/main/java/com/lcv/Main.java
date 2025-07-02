d57215192fbaa07624b02636648dc51cf27883ad
LeverClient <ah19890000@gmail.com>
2025-07-02 18:46:06 -0400

    Filler Code

---
+++ src/main/java/com/lcv/Main.java
@@ -0,0 +1,27 @@
+package com.lcv;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Main extends ListenerAdapter
+{
+    public static void main(String[] args)
+    {
+        JDA jda = JDABuilder.create(
+                System.getenv("BOTKEY"),
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.GUILD_MESSAGES)
+                .addEventListeners(new Main())
+                .build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (!event.getAuthor().isBot())
+            event.getMessage().reply("hey!").queue();
+    }
+}
