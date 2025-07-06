bedb9186959db4b932fb19bd25dc2920eb951f8c
syl <81616776+syl127@users.noreply.github.com>
2025-07-06 02:48:50 -0600

    add valid field

--- src/main/java/com/lcv/commands/hypixel/HypixelPlayerData.java
+++ src/main/java/com/lcv/commands/hypixel/HypixelPlayerData.java
@@ -9,11 +9,25 @@ public class HypixelPlayerData
 
     protected JsonObject stats;
 
+    boolean valid = true;
+
     public HypixelPlayerData(JsonObject json)
     {
+        if (!json.has("player")) {
+            valid = false;
+            return;
+        }
+
         player = json.get("player").getAsJsonObject();
+
+        if (!player.has("stats")) {
+            valid = false;
+            return;
+        }
+
         stats = player.get("stats").getAsJsonObject();
         System.out.println("wow!");
+
     }
 
     // TODO: dedicated readers for each game here maybe?
