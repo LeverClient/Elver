1acf5ffa488ffa671e948f27341dbfd22c750da6
syl <81616776+syl127@users.noreply.github.com>
2025-07-06 02:32:06 -0600

    read data a little

--- src/main/java/com/lcv/commands/hypixel/HypixelPlayerData.java
+++ src/main/java/com/lcv/commands/hypixel/HypixelPlayerData.java
@@ -1,11 +1,20 @@
 package com.lcv.commands.hypixel;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class HypixelPlayerData
 {
+    protected JsonObject player;
+
+    protected JsonObject stats;
+
     public HypixelPlayerData(JsonObject json)
     {
-
+        player = json.get("player").getAsJsonObject();
+        stats = player.get("stats").getAsJsonObject();
+        System.out.println("wow!");
     }
+
+    // TODO: dedicated readers for each game here maybe?
 }
