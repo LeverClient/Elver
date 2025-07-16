4ec6e4f6201ef66e488749b543f10b2097fbc821
syl <81616776+syl127@users.noreply.github.com>
2025-07-16 01:12:57 -0600

    declare skins in json

--- src/main/java/com/lcv/util/StatsSkins.java
+++ src/main/java/com/lcv/util/StatsSkins.java
@@ -1,6 +1,17 @@
 package com.lcv.util;
 
+import com.lcv.Main;
+import net.dv8tion.jda.internal.utils.IOUtil;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class StatsSkins {
@@ -14,12 +25,54 @@ public class StatsSkins {
 
     public static Skin none = new Skin();
 
-    public static Skin pink = new Skin(new Color(255, 180, 235), 15)
-            .applyToUser("ddf13e436ccc4790bb49912913bf7d77"); // syl127 (me)
+    public static Skin parseSkin(JSONObject skin) {
+        JSONArray users = skin.getJSONArray("Users");
+        Object primaryColor = skin.opt("Primary");
+        Object secondaryColor = skin.opt("Secondary");
+        Object defaultColor = skin.opt("Default");
 
-    public static Skin idk = new Skin(11, 8)
-            .applyToUser("f7a757d51d6d408eb54ea7230b95ae15"); // lever client
+        Skin statsSkin = new Skin(primaryColor, secondaryColor, defaultColor);
 
+        if (skin.has("Colors") && !skin.isNull("Colors")) {
+            JSONArray colors = skin.getJSONArray("Colors");
+
+            int i = 0;
+            for (Object color : colors) {
+                statsSkin.setColor(i++, color);
+            }
+        }
+
+        // apply users
+        for (Object user : users) {
+            String uuid = ((JSONArray) user).getString(0);
+
+            statsSkin.applyToUser(uuid);
+        }
+
+        return statsSkin;
+    }
+
+    static {
+        JSONObject skinsJson;
+        try (InputStream skinsStream = Main.class.getResourceAsStream("/Skins.json")) {
+            assert skinsStream != null;
+
+            skinsJson = new JSONObject(new String(skinsStream.readAllBytes()));
+        } catch (IOException e) {
+            System.out.println("failure to do thing");
+            throw new RuntimeException(e);
+        }
+
+        ArrayList<Skin> skins = new ArrayList<>();
+        for (String key : skinsJson.keySet()) {
+            JSONObject skinJson = skinsJson.getJSONObject(key);
+
+            Skin skin = parseSkin(skinJson);
+            skins.add(skin);
+        }
+
+        System.out.println("skins done!");
+    }
 
     public static class Skin {
         public Color[] colors = new Color[16];
@@ -31,13 +84,29 @@ public class StatsSkins {
             colors[color_secondary] = getColorFromGeneric(secondary);
         }
 
+        public Skin(Object main, Object secondary, Object def) {
+            colors[color_main] = getColorFromGeneric(main);
+            colors[color_secondary] = getColorFromGeneric(secondary);
+            colors[colors.length-1] = getColorFromGeneric(def);
+        }
+
         private Color getColorFromGeneric(Object gen) {
             if (gen instanceof Integer) {
                 gen = FontRenderer.defaultColors[(int) gen];
             }
 
-            if (gen != null && !(gen instanceof Color)) {
-                throw new IllegalArgumentException("Invalid generic type for getColorFromGeneric(). Must be either Integer, Color, or null");
+            else if (gen instanceof int[] gen_arr) {
+                gen = new Color(gen_arr[0], gen_arr[1], gen_arr[2]);
+            }
+
+            else if (gen instanceof JSONArray gen_arr) {
+                gen = new Color(gen_arr.getInt(0), gen_arr.getInt(1), gen_arr.getInt(2));
+            } else if (gen == JSONObject.NULL) {
+                gen = null;
+            }
+
+            else if (gen != null && !(gen instanceof Color)) {
+                throw new IllegalArgumentException("Invalid generic type for getColorFromGeneric(). Must be either Integer, Color, int[], or null");
             }
 
             return (Color) gen;
